f8b6d93472c543d87673ce0501b975b96b231e32
joel <joelcittar@gmail.com>
2013-06-04 13:23:57 -0300

    DEV: comentario borrado

--- server.js
+++ server.js
@@ -33,4 +33,3 @@ config.routesBootstrap(app,express);
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
 });
-//test
